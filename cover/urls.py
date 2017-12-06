c5fc92cb44f9f633d4e66c4bbefc3a4e56afde30
Lee HyunJoo <wayhome250@gmail.com>
2017-12-06 23:47:28 +0900

    index 뷰 구현

--- cover/urls.py
+++ cover/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from cover import views
+
 app_name = 'cover'
 
 urlpatterns = [
-
+    path('index/', views.index, name='index'),
 ]
--- orly/urls.py
+++ orly/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cover/', include('cover.urls', namespace='cover')),
+    path('', lambda r: redirect('cover:index')),
 ]
